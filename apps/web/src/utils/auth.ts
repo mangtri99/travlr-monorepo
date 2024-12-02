b31018d9807714d6a368b29154e79773616a9b8f
Mang Tri <mangtri93@gmail.com>
2024-12-02 17:44:38 +0800

    trigger

--- apps/web/src/utils/auth.ts
+++ apps/web/src/utils/auth.ts
@@ -4,9 +4,7 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 
 // types imports
 import type { NextAuthConfig, Session, User } from 'next-auth';
-// import type { UserType, UserResponseType } from "@/types/user";
 import { AdapterUser } from 'next-auth/adapters';
-// import { CredentialsType, SocialCredentialsType } from "@/types/login";
 import { JWT } from 'next-auth/jwt';
 import axios from 'axios';
 
