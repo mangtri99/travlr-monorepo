9f3003f899049eb0475db0dd11d24ba3d4b254fb
Mang Tri <mangtri93@gmail.com>
2024-12-05 12:06:07 +0800

    fix: session auth

--- apps/web/src/app/product/create/page.tsx
+++ apps/web/src/app/product/create/page.tsx
@@ -1,9 +1,14 @@
 import ProductForm from '../_components/ProductForm';
+import { auth } from '../../../utils/auth';
+import { SessionProvider } from 'next-auth/react';
 
-export default function Index() {
+export default async function Index() {
+  const session = await auth();
   return (
-    <div className="flex flex-col">
-      <ProductForm />
-    </div>
+    <SessionProvider session={session}>
+      <div className="flex flex-col">
+        <ProductForm />
+      </div>
+    </SessionProvider>
   );
 }
